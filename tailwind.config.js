5413edcc4afe4ad0a11d49a8733946b0ad78ac01
TheRockYT <91672537+TheRockYT@users.noreply.github.com>
2023-04-30 10:57:15 +0000

    no-unescaped-entities build error fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,4 @@ module.exports = {
     },
   },
   plugins: [],
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
