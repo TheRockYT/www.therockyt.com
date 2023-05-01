18830b8447a22d4e6e4fbda13467baea40fed775
TheRockYT <91672537+TheRockYT@users.noreply.github.com>
2023-05-01 16:49:29 +0000

    Type error: 'searchParams' is possibly 'null'.

--- app/link/page.tsx
+++ app/link/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  var to = searchParams.get("to");
+  var to = searchParams.get("to")!!;
   if (to !== null && (to.startsWith("https://") || to.startsWith("http://"))) {
     return (
       <div>
