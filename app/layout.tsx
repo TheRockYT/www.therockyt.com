68fcf1fb6c3ed707e6efb5a3675517f5bf9f2ad2
TheRockYT <mail@therockyt.com>
2023-05-06 19:56:55 +0200

    Separated NavComponent & FooterComponent

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,15 @@
-"use client";
-
-import Link from "next/link";
-
-import { useRouter, usePathname } from "next/navigation";
-import { useState } from "react";
-
 import "@/style/general.css";
 import "@/style/navigation.css";
 import "@/style/site.css";
+
+import NavComponent from "@/components/NavComponent";
+import FooterComponent from "@/components/FooterComponent";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-  const pathname = usePathname();
-
-  const [isActive, setIsActive] = useState(false);
-
-  function toggleMenu() {
-    setIsActive(!isActive);
-  }
-  function setMenu(active: boolean) {
-    setIsActive(active);
-  }
-  async function pushToSite(site: string) {
-    router.push(site);
-    setMenu(false);
-  }
-  const navigation = [
-    ["Home", "/"],
-    ["DirectBans", "/directbans"],
-    ["Sync", "/sync"],
-    ["FSpawn", "/fspawn"],
-    ["Cloud", "/advancedcloud"],
-    ["ShutUpPC", "/shutuppc"],
-    ["GitHub", "/link?to=https://github.com/TheRockYT"],
-  ];
-  const footer = [
-    ["Home", "/"],
-    ["API", "/api"],
-    ["Terms and Conditions", "/terms"],
-    ["Privacy Policy", "/privacypolicy"],
-    ["Contact", "contact"],
-    ["GitHub", "/link?to=https://github.com/TheRockYT"],
-  ];
   return (
     <html>
       <head>
@@ -60,63 +24,9 @@ export default function RootLayout({
         <title>TheRockYT</title>
       </head>
       <body>
-        <nav className={isActive ? "enabled" : ""}>
-          <img
-            src="/img/icons/menu_open.png"
-            alt="X"
-            id="nav_open"
-            className={isActive ? "" : "enabled"}
-            onClick={toggleMenu}
-          />
-          <img
-            src="/img/icons/menu_close.png"
-            alt="X"
-            id="nav_close"
-            className={isActive ? "enabled" : ""}
-            onClick={toggleMenu}
-          />
-          <ul id="nav_list" className={isActive ? "enabled" : ""}>
-            {navigation.map((item) => (
-              <li key={item[0]}>
-                <Link
-                  href={item[1]}
-                  passHref
-                  onClick={() => {
-                    setMenu(false);
-                  }}
-                  className={pathname === item[1] ? "active" : ""}
-                >
-                  {item[0]}
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <img
-            src="/img/therockyt.png"
-            onClick={() => {
-              router.push("/");
-            }}
-          />
-        </nav>
+        <NavComponent />
         <main>{children}</main>
-        <footer>
-          <ul>
-            {footer.map((item) => (
-              <li key={item[0]}>
-                <Link
-                  href={item[1]}
-                  passHref
-                  onClick={() => {
-                    setMenu(false);
-                  }}
-                  className={pathname === item[1] ? "active" : ""}
-                >
-                  {item[0]}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </footer>
+        <FooterComponent />
       </body>
     </html>
   );
