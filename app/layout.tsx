525c6334031fd09619af90eeda46fb5bba6c4875
TheRockYT <jonasfelsen@gmail.com>
2023-05-07 18:10:51 +0200

    Optimized PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="TheRockYT" />
+        <meta name="description" content="TheRockYT's Website" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <link rel="apple-touch-icon" href="/img/therockyt.png" />
         <meta name="application-name" content="TheRockYT" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
