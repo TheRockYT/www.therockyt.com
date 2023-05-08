5edde483f0aa59f2574a15e6e79108b291ca8af0
TheRockYT <mail@therockyt.com>
2023-05-08 11:14:12 +0200

    Added Protal

---
+++ app/portal/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "Portal - TheRockYT",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Portal</h1>
+      <br />
+      <br />
+      <p>The Portal is currently not available.</p>
+    </div>
+  );
+}
